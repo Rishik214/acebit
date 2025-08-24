132fc9af4c86a5a23c6a49e3ad7eb4630a7f7798
Rishik214 <rishikesh20736@gmail.com>
2025-08-24 22:54:44 +0530

    Update Navbar.js

--- acebit-main/src/components/nav/Navbar.js
+++ acebit-main/src/components/nav/Navbar.js
@@ -76,7 +76,7 @@ const Navbar = () => {
               <div className={`text-xs sm:text-sm lg:text-sm font-medium ${
                 scrolled ? 'text-slate-600' : 'text-blue-100'
               }`}>
-                Association of Civil Engineers
+                Association Of Civil Engineers
               </div>
             </div>
           </div>
